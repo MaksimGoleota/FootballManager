28c60a87b95639ea232e8db962d0421f34a28104
MaksimGoleota <74358982+MaksimGoleota@users.noreply.github.com>
2021-10-18 15:19:33 +0300

    Initial commit

---
+++ app/src/main/java/com/merio/footballManager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.merio.footballManager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
